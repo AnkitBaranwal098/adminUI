ed2a3fe1b526eeb77bcc85c5e6637100bd0e4f81
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-11-09 00:56:00 +0530

    Fetched data from server

--- src/app/admin-ui/admin-ui.component.ts
+++ src/app/admin-ui/admin-ui.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
+import {User} from '../User.interface'
 @Component({
   selector: 'app-admin-ui',
   templateUrl: './admin-ui.component.html',
   styleUrls: ['./admin-ui.component.css']
 })
-export class AdminUIComponent {
+export class AdminUIComponent 
+{
+  userData : User[] = []
+  constructor(private http:HttpClient){}
+
+  ngOnInit(){
+    this.fetchDataFromUrl()
+  }
+  handleClick(){
+    
+    this.fetchDataFromUrl('https://reqres.in/api/users?page=2')
+  }
+  fetchDataFromUrl(url='https://reqres.in/api/users'){
+    this.http.get<{data: User[]}>(url).subscribe((responseData)=>{
+      this.userData = responseData['data']
+    })
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { AdminUIComponent } from './admin-ui/admin-ui.component';
@@ -10,7 +11,8 @@ import { AdminUIComponent } from './admin-ui/admin-ui.component';
     AdminUIComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
