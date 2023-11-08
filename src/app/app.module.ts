e243d1113a80c2ee0e9ee3bd32db383ceef536e8
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-11-09 00:12:22 +0530

    AdminUI in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminUIComponent } from './admin-ui/admin-ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminUIComponent
   ],
   imports: [
     BrowserModule
